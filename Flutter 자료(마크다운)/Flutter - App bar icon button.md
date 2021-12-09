140a5524cac1f4cd1fc100fbcf705a62f929256d
Bepo0328 <82445568+Bepo0328@users.noreply.github.com>
2021-12-09 21:38:24 +0900

    StudyToFlutter: Flutter - 앱바 메뉴

---
+++ Flutter 자료(마크다운)/Flutter - App bar icon button.md
@@ -0,0 +1,7 @@
+Flutter - App bar icon button
+========
+
+# 1. App bar icon Button
+- leading: 아이콘 버튼이나 간단한 위젯을 왼쪽에 배치할 때
+- actions: 복수의 아이콘 버튼 등을 오른쪽에 배치할 때
+- onPressed: 함수의 형태로 일반 버튼이나 아이콘 버튼을 터치했을 때 일어나는 이벤트를 정의 한곳
--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,131 +1,51 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "BBANTO",
-      home: Grade(),
+    return MaterialApp(
+      title: "Appbar",
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: MyHomePage(),
     );
   }
 }
 
-class Grade extends StatelessWidget {
-  const Grade({Key? key}) : super(key: key);
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.amber[800],
       appBar: AppBar(
-        title: const Text("BBANTO"),
-        backgroundColor: Colors.amber[700],
+        title: Text("Appbar icon menu"),
         centerTitle: true,
         elevation: 0.0,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(30.0, 40.0, 0.0, 0.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage("assets/cat2.gif"),
-                radius: 60.0,
-              ),
-            ),
-            Divider(
-              height: 60.0,
-              color: Colors.grey[850],
-              thickness: 0.5,
-              endIndent: 30.0,
-            ),
-            const Text(
-              "Name",
-              style: TextStyle(color: Colors.white, letterSpacing: 2.0),
-            ),
-            const SizedBox(
-              height: 10.0,
-            ),
-            const Text(
-              "BBANTO",
-              style: TextStyle(
-                  color: Colors.white,
-                  letterSpacing: 2.0,
-                  fontSize: 28.0,
-                  fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(
-              height: 30.0,
-            ),
-            const Text(
-              "BBANTO POWER LEVEL",
-              style: TextStyle(color: Colors.white, letterSpacing: 2.0),
-            ),
-            const SizedBox(
-              height: 10.0,
-            ),
-            const Text(
-              "14",
-              style: TextStyle(
-                  color: Colors.white,
-                  letterSpacing: 2.0,
-                  fontSize: 28.0,
-                  fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(
-              height: 30.0,
-            ),
-            Row(
-              children: const [
-                Icon(Icons.check_circle_outline),
-                SizedBox(
-                  width: 10.0,
-                ),
-                Text(
-                  "using lightsaber",
-                  style: TextStyle(fontSize: 16.0, letterSpacing: 1.0),
-                ),
-              ],
-            ),
-            Row(
-              children: const [
-                Icon(Icons.check_circle_outline),
-                SizedBox(
-                  width: 10.0,
-                ),
-                Text(
-                  "face hero tattoo",
-                  style: TextStyle(fontSize: 16.0, letterSpacing: 1.0),
-                ),
-              ],
-            ),
-            Row(
-              children: const [
-                Icon(Icons.check_circle_outline),
-                SizedBox(
-                  width: 10.0,
-                ),
-                Text(
-                  "fire flames",
-                  style: TextStyle(fontSize: 16.0, letterSpacing: 1.0),
-                ),
-              ],
-            ),
-            Center(
-              child: CircleAvatar(
-                backgroundImage: const AssetImage("assets/cat1.png"),
-                radius: 40.0,
-                backgroundColor: Colors.amber[800],
-              ),
-            ),
-          ],
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: () {
+            print("menu button is clicked");
+          },
         ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () {
+              print("Shopping cart button is clicked");
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              print("search button is clicked");
+            },
+          ),
+        ],
       ),
     );
   }
