55c6605c20fd3a35a09ea191a982587ebdf5dc68
Bepo0328 <82445568+Bepo0328@users.noreply.github.com>
2021-12-12 22:26:57 +0900

    StudyToFlutter: Flutter - Snack bar

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -27,26 +27,34 @@ class MyPage extends StatelessWidget {
         title: Text("Snack Bar"),
         centerTitle: true,
       ),
-      body: Builder(
-        builder: (BuildContext ctx) {
-          return Center(
-            child: FlatButton(
-              child: Text(
-                "Show me",
-                style: TextStyle(
-                  color: Colors.white,
+      body: MySnackBar(),
+    );
+  }
+}
+
+class MySnackBar extends StatelessWidget {
+  const MySnackBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: RaisedButton(
+          child: Text("Show me"),
+          onPressed: () {
+            Scaffold.of(context).showSnackBar(
+              SnackBar(
+                content: Text(
+                  "Hello",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
                 ),
+                backgroundColor: Colors.teal,
+                duration: Duration(milliseconds: 1000),
               ),
-              color: Colors.red,
-              onPressed: () {
-                Scaffold.of(ctx).showSnackBar(SnackBar(
-                  content: Text("Hello"),
-                ));
-              },
-            ),
-          );
-        },
-      ),
+            );
+          }),
     );
   }
 }
