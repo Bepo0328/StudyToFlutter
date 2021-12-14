d961a58c42de8c09252a3921da7eba4fa22fea4d
Bepo0328 <82445568+Bepo0328@users.noreply.github.com>
2021-12-14 21:56:11 +0900

    StudyToFlutter: Flutter - Navigator & pushNamed

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/screen_a.dart';
+import 'package:flutter_app/screen_b.dart';
+import 'package:flutter_app/screen_c.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -8,50 +11,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "First page",
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: FirstPage(),
+      initialRoute: "/",
+      routes: {
+        "/" : (context) => const ScreenA(),
+        "/b" : (context) => const ScreenB(),
+        "/c" : (context) => const ScreenC(),
+      },
     );
   }
 }
 
-class FirstPage extends StatelessWidget {
-  const FirstPage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context2) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("First page"),
-      ),
-      body: Center(
-        child: RaisedButton(
-          child: Text("Go to the Second page"),
-          onPressed: () {
-            Navigator.push(context2,
-                MaterialPageRoute(builder: (context) => SecondPage()));
-          },
-        ),
-      ),
-    );
-  }
-}
-
-class SecondPage extends StatelessWidget {
-  const SecondPage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext ctx) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Second page"),
-      ),
-      body: Center(
-        child:
-            RaisedButton(child: Text("Go to the First page"), onPressed: () {
-              Navigator.pop(ctx);
-            }),
-      ),
-    );
-  }
-}
