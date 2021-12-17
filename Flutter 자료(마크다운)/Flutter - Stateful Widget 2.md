87254d82242e25447fb9208c02d31b92331e1e26
Bepo0328 <82445568+Bepo0328@users.noreply.github.com>
2021-12-17 23:39:13 +0900

    StudyToFlutter: Flutter - Stateful widget part 2

---
+++ Flutter 자료(마크다운)/Flutter - Stateful Widget 2.md
@@ -0,0 +1,14 @@
+Flutter - Stateful Widget 2
+========
+
+# 1. Stateful Widget
+
+|Input Data(external)|Input Data(external)|
+|---|---|
+|Statefull and Stateless Widget|Stateful widget<br/>State|
+|Build method|Build method|
+|Renders UI|Renders UI|
+
+## 1.1. State
+	- Child 위젯의 생성자를 통해서 데이터가 전달 될 때
+	- Internal state가 바뀔 때
--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -2,17 +2,57 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  int counter = 0;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
       home: Scaffold(
         appBar: AppBar(),
         body: Center(
-          child: Text(
-            "France",
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("You have pushed the button this many times:"),
+              Text("$counter", style: Theme.of(context).textTheme.headline4),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  FloatingActionButton(
+                    child: Icon(Icons.add),
+                    onPressed: () {
+                      setState(() {
+                        counter++;
+                        print("$counter");
+                      });
+                    },
+                  ),
+                  SizedBox(
+                    width: 20.0,
+                  ),
+                  FloatingActionButton(
+                    child: Icon(Icons.remove),
+                    onPressed: () {
+                      setState(() {
+                        counter--;
+                        print("$counter");
+                      });
+                    },
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
