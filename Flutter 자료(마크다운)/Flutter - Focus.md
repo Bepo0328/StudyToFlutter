5cb2f7eb342e07721dd073222fa8062297f50d2d
Bepo0328 <82445568+Bepo0328@users.noreply.github.com>
2021-12-19 00:12:02 +0900

    StudyToFlutter: Flutter - Login & Dice App 3

---
+++ Flutter 자료(마크다운)/Flutter - Focus.md
@@ -0,0 +1,6 @@
+Flutter - Focus
+========
+
+# 1. Focus
+	FocusNode: 포커스를 받는 특정 위젯을 식별
+    FocusScope: 어떤 위젯들까지 포커스를 받는지 범위를 나타냄
--- Login_Dice/lib/dice.dart
+++ Login_Dice/lib/dice.dart
@@ -1,18 +1,80 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
-class Dice extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'dart:math';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class Dice extends StatefulWidget {
   const Dice({Key? key}) : super(key: key);
+
+  @override
+  State<Dice> createState() => _DiceState();
+}
+
+class _DiceState extends State<Dice> {
+  int leftDice = 1;
+  int rightDice = 1;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Dice game"),
-        backgroundColor: Colors.blue,
+        backgroundColor: Colors.redAccent,
       ),
+      backgroundColor: Colors.redAccent,
       body: Center(
-        child: Text("Dice game page"),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: EdgeInsets.all(32.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Image.asset("image/dice$leftDice.png"),
+                  ),
+                  SizedBox(
+                    width: 20.0,
+                  ),
+                  Expanded(
+                    child: Image.asset("image/dice$rightDice.png"),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 60.0,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  leftDice = Random().nextInt(6) + 1;
+                  rightDice = Random().nextInt(6) + 1;
+                });
+                showToast("Left Dice: {$leftDice}, Right Dice: {$rightDice}");
+              },
+              child: Icon(
+                Icons.play_arrow,
+                color: Colors.white,
+                size: 50.0,
+              ),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.orangeAccent,
+                minimumSize: Size(100.0, 60.0),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
+}
+
+void showToast(String message) {
+  Fluttertoast.showToast(
+      msg: message,
+      backgroundColor: Colors.white,
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.BOTTOM);
 }
