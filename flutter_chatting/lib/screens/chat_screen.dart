3a181e91eca9a3dacb59d8ba151477cf6bcef660
Bepo0328 <82445568+Bepo0328@users.noreply.github.com>
2022-02-19 22:25:11 +0900

    StudyToFlutter: Flutter - Chatting App 8 - Send chat messages

--- flutter_chatting/lib/screens/chat_screen.dart
+++ flutter_chatting/lib/screens/chat_screen.dart
@@ -1,6 +1,7 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_chatting/chatting/chat/message.dart';
+import 'package:flutter_chatting/chatting/chat/new_message.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({Key? key}) : super(key: key);
@@ -49,31 +50,15 @@ class _ChatScreenState extends State<ChatScreen> {
           ),
         ],
       ),
-      body: StreamBuilder(
-        stream: FirebaseFirestore.instance
-            .collection('chats/3sEMEdX5sEDnNuUrH4WN/message')
-            .snapshots(),
-        builder: (BuildContext context,
-            AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-          final docs = snapshot.data!.docs;
-          return ListView.builder(
-            itemCount: docs.length,
-            itemBuilder: (context, index) {
-              return Container(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  docs[index]['text'],
-                  style: const TextStyle(fontSize: 20.0),
-                ),
-              );
-            },
-          );
-        },
+      body: Container(
+        child: Column(
+          children: const [
+            Expanded(
+              child: Messages(),
+            ),
+            NewMessage(),
+          ],
+        ),
       ),
     );
   }
